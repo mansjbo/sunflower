750ebf853c920ee301fa845790005b01d0fd5543
mansjbo <mansjbo@aol.com>
2025-07-08 22:21:45 +0200

    Netlify Shape

--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const serverless = require("serverless-http");
 const app = express();
 
 // Middleware to parse JSON
@@ -13,14 +13,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/users", require("./routes/users"));
-app.use("/brands", require("./routes/brands"));
-app.use("/categories", require("./routes/categories.js"));
-app.use("/items", require("./routes/items.js"));
-app.use("/suppliers", require("./routes/suppliers.js"));
-app.use("/supplies", require("./routes/supplies.js"));
-app.use("/sales", require("./routes/sales.js"));
-app.use("/test", require("./routes/testapi"));
+app.use("/users", require("../routes/users.js"));
+app.use("/brands", require("../routes/brands.js"));
+app.use("/categories", require("../routes/categories.js"));
+app.use("/items", require("../routes/items.js"));
+app.use("/suppliers", require("../routes/suppliers.js"));
+app.use("/supplies", require("../routes/supplies.js"));
+app.use("/sales", require("../routes/sales.js"));
+app.use("/test", require("../routes/testapi.js"));
 // app.get('/', (req,res)=>{
 //     console.log("Requesting.....");
 //     res.status(201).send("Successfully Requested hhhhhhhhhh")
@@ -32,7 +32,10 @@ app.use("/test", require("./routes/testapi"));
 //     res.status(500).json({ message: 'خطأ، فشل العملية', status: 'fail' });
 // }
 
-const PORT = 4496;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
+
+// const PORT = 4496;
+// app.listen(PORT, () => {
+// 	console.log(`Server is running on port ${PORT}`);
+// });
